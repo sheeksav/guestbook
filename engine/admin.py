1c59da8fd3770fc56b7b8958d9d7f6bd0e3f4497
sheeksav <andy.sheeks@gmail.com>
2014-06-21 11:31:52 -0400

    Added Guest and Event models with admin.

--- engine/admin.py
+++ engine/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from models import Event, Guest
 
 # Register your models here.
+admin.site.register(Event)
+admin.site.register(Guest)
--- engine/models.py
+++ engine/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Guest(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return u'%s %s' % (self.first_name, self.last_name)
+
